fb3ee4c16b36f34d4f9be67dfc85714fa6e90edb
Ruslan <foyyui@gmail.com>
2025-07-01 21:38:34 +0300

    add infinite scroll for products list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
 import ProductList from "@/entites/product/ui/ProductList";
 import { Suspense } from "react";
 
-async function getAllProducts() {
-  try {
-    const res = await fetch('https://fakestoreapi.in/api/products?limit=150', {
-      next: { revalidate: 3600 }
-    });
-
-    if (!res.ok) {
-      console.error(`Failed to fetch all products: ${res.status} ${res.statusText}`);
-    };
-
-    const data = await res.json();
-    return data.products || [];
-  } catch (error) {
-    console.error("Error in getAllProducts (server):", error);
-    return [];
-  }
-};
-
 export default async function HomePage() {
-  const allProducts = await getAllProducts();
-
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <ProductList products={allProducts}/>
+        <ProductList />
       </Suspense>
     </>
   );
--- src/entites/product/ui/ProductList.tsx
+++ src/entites/product/ui/ProductList.tsx
@@ -1,19 +1,63 @@
-import { ProductProps } from "../model/types";
-import ProductCard from "./ProductCard";
-interface ProductListProps {
-  products: ProductProps[];
-}
+"use client";
 
-export default function ProductList({ products }: ProductListProps) {
-  return (
-    <>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {products.map((product) => (
-          <li key={product.id} className="w-full">
-            <ProductCard product={product} />
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import ProductCard from "./ProductCard";
+import { RootState } from "@/lib/store";
+import { fetchProducts } from "@/features/products/productsSlice";
+import { useAppDispatch } from "@/hooks/useRedux";
+import ProductCardSkeleton from "./ProductCardSkeleton";
+import { selectProducts } from "@/features/products/productsSelectors";
+
+const PAGE_SIZE = 10;
+
+export default function ProductList() {
+  const dispatch = useAppDispatch();
+  const { products, isLoading, error } = useSelector(selectProducts);
+
+  const [visibleCount, setVisibleCount] = useState(PAGE_SIZE);
+
+  useEffect(() => {
+    if (products.length === 0) {
+      dispatch(fetchProducts());
+    }
+  }, [dispatch, products.length]);
+
+  useEffect(() => {
+    function onScroll() {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+        visibleCount < products.length
+      ) {
+        setVisibleCount((count) => count + PAGE_SIZE);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [visibleCount, products.length]);
+
+  if (isLoading) {
+    return (
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {Array.from({ length: PAGE_SIZE }).map((_, index) => (
+          <li key={index}>
+            <ProductCardSkeleton />
           </li>
         ))}
       </ul>
-    </>
+    );
+  };
+
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {products.slice(0, visibleCount).map((product) => (
+        <li key={product.id}>
+          <ProductCard product={product} />
+        </li>
+      ))}
+    </ul>
   );
 }
